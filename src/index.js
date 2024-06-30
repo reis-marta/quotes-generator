591af71b042a8ec8858e5a788107e8cbdc193732
reis-marta <163763026+reis-marta@users.noreply.github.com>
2024-06-30 16:58:06 +0100

    Add-basic-JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Quote will appear here...",
+    autoStart: true,
+    cursor: "",
+    delya: 1,
+  });
+}
+
+quoteFormElement = document.querySelector("#quote-generator");
+quoteFormElement.addEventListener("submit", generateQuote);
